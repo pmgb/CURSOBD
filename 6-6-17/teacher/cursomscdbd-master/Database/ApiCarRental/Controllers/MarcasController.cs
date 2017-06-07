91f9f7fdd54ea1844453ae6d232db29cd34add65
Samuel Ramírez Afonso <samuel_ra93@hotmail.com>
2017-06-07 20:25:27 +0100

    clase 7-6-17

---
+++ 6-6-17/teacher/cursomscdbd-master/Database/ApiCarRental/Controllers/MarcasController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiCarRental.Controllers
+{
+    public class MarcasController : ApiController
+    {
+        // GET: api/Marcas
+        public RespuestaAPI Get()
+        {
+            RespuestaAPI resultado = new RespuestaAPI();
+            List<Marca> listaMarcas = new List<Marca>();
+            try
+            {
+                Db.Conectar();
+
+                if (Db.EstaLaConexionAbierta())
+                {
+                    listaMarcas = Db.GetMarcas();
+                }
+                resultado.error = "";
+                Db.Desconectar();
+            }
+            catch 
+            {
+                resultado.error = "Se produjo un error";
+            }
+
+            resultado.totalElementos = listaMarcas.Count;
+            resultado.dataMarcas =  listaMarcas;
+            return resultado;
+        }
+
+
+
+
+
+
+
+
+
+        // GET: api/Marcas/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Marcas
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Marcas/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Marcas/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
